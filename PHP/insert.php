da83c42e5311753152b051fabb092874d96e66ab
MichaelOfosuSiaw45 <67522677+MichaelOfosuSiaw45@users.noreply.github.com>
2025-03-06 11:14:38 +0000

    Trying database connection

---
+++ PHP/insert.php
@@ -0,0 +1,17 @@
+<?php
+include 'db_connect.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
